eb082093cb38e1dbf623c0c21d75174779056f4b
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-05-22 14:48:43 +0600

    Updated home screen :zap:

--- lib/features/screens/home/components/body.dart
+++ lib/features/screens/home/components/body.dart
@@ -14,6 +14,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
       child: Center(
         child: Column(
           children: [
